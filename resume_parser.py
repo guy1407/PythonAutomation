2ebc8e4ecf0afe5cbc53d9b978bcc3d7668ce6c0
Guy Refael Cohen <guy1407@gmail.com>
2021-10-11 13:42:07 +0300

    extract resume finished 02:16:20

--- resume_parser.py
+++ resume_parser.py
@@ -18,39 +18,57 @@ def parse_content(vsFileContent):
     # load the language model
     nlp = spacy.load("en_core_web_sm")
 
-    # Helper variables
-    result_dict = {'name': [], 'phone': [], 'email': [], 'skills': []}
-    names = []
-    phones = []
-    emails = []
-    skills = []
-
     # Some skills that we look in resumes:
+    
     skillset = re.compile(
-        'python|java|sql|hadoop|tableau|c|c++|c#|vb|vb.net|asp|asp.net')
+        'c\+\+|python|java|sql|hadoop|tableau|c#|vb|vb.net|asp|asp.net|c')
 
     # phone_num credit https://stackoverflow.com/a/3868861
     phone_num = re.compile(
         "(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})"
     )
 
+    sEmailAddress = ""
+    sPersonName = ""
+    
     doc = nlp(vsFileContent)
 
     sPersonName = [
         entity.text for entity in doc.ents if entity.label_ == "PERSON"][0]
+
     print("name = " + sPersonName)
 
-    sEmailAddress = [word for word in doc if word.like_email == True][0]
-    print("email = " + sEmailAddress)
+    #sEmailAddress = [word for word in doc if word.like_email == True][0]
+    for word in doc:
+        if word.like_email == True:
+            sEmailAddress = str(word)
+            break
+
+    print("email = [" + sEmailAddress + "]")
 
     sPhoneNumber = str(re.findall(phone_num, vsFileContent.lower()))
+    
     skills_list = re.findall(skillset, vsFileContent.lower())
+
     unique_skills_list = str(set(skills_list))
+
     names.append(sPersonName)
+
     emails.append(sEmailAddress)
+
     phones.append(sPhoneNumber)
+
     skills.append(unique_skills_list)
+
     print("Extraction completed successfully!!!")
+
+
+# Helper variables
+result_dict = {'name': [], 'phone': [], 'email': [], 'skills': []}
+names = []
+phones = []
+emails = []
+skills = []
 
 
 for sPDFFilename in os.listdir('resumes/'):
@@ -71,3 +89,14 @@ for sPDFFilename in os.listdir('resumes/'):
 
         if (sFileContent != ""):
             parse_content(sFileContent)
+
+result_dict['name'] = names
+result_dict['phone'] = phones
+result_dict['email'] = emails
+result_dict['skills'] = skills
+
+result_df = pd.DataFrame(result_dict)
+
+result_df.to_excel("summary.xlsx")
+
+print("Summary created !")
